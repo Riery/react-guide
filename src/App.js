a1e9d9c3a93e2ad22ff38c409dc4581c0bb77e08
Riery <riery9@gmail.com>
2020-03-26 16:21:13 +0100

    function pass to component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,44 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  state
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState(
+      {
+        persons: [
+          { name: newName, age: 28 },
+          { name: 'Manu', age: 29 },
+          { name: 'Stephanie', age: 27 },
+        ]
+      }
+    )
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26" />
+        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Max!')}>
+          My Hobbies: Racing
+          </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
