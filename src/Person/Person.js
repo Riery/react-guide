b7e0de7b9e86d3671e0f4701b30701c71ce28977
Riery <riery9@gmail.com>
2020-03-26 15:41:59 +0100

    props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
     return (
-        <p>I'm a Person!</p>
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
     );
 }
 
