98fb38bf8265e275f1499ce91a3d630dcaffbaf5
Riery <riery9@gmail.com>
2020-03-31 15:18:00 +0200

    5.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 },
-    ]
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Stephanie', age: 26 },
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -23,24 +24,68 @@ class App extends Component {
     )
   }
 
+  nameChangedHandler = (event, index) => {
+    const person = { ...this.state.persons[index] };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[index] = person;
+    this.setState({ persons: persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              changed={(event) => this.nameChangedHandler(event, index)}
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
+          })}
+        </div>
+      );
+      style.backgroundColor = "red";
+      style[':hover'] = { backgroundColor: 'salmon' };
+    }
+
+    const classes = [];
+    if (this.state.persons.length < 3) {
+      classes.push('red');
+    }
+    if (this.state.persons.length < 2) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age} 
-          click={this.switchNameHandler.bind(this, 'Max!')}>
-          My Hobbies: Racing
-          </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}> Toggle Persons </button>
+        {persons}
       </div>
     );
   }
